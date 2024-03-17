cbe767c23d0bfdbfd7c4a98756d1337b56e3577a
dmitrikll <dgalytskyi@gmail.com>
2024-03-17 17:44:33 +0200

    added task02

---
+++ src/task02/Main.java
@@ -0,0 +1,15 @@
+package task02;
+
+/*
+import task02.var0201.SameName;
+import task02.var0202.SameName;
+*/
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("It is impossible to import 2 classes with the same name. \n" +
+                "Error: \"[CLASS] is already defined in a single-type import\".");
+
+    }
+}
---
+++ src/task02/var0201/SameName.java
@@ -0,0 +1,6 @@
+package task02.var0201;
+
+public class SameName {
+    String employeePosition;
+    double employeeSalary;
+}
